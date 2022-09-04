e7dfd0349a07babc0b1c7ea3b4b94d0ed40c6568
barlette <leobarlette@gmail.com>
2022-09-04 17:31:12 -0300

    problem solved, I just made a reference to the string vector in the start instead of borrowing it

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use std::cmp;
 
 fn longest_common_prefix(strs: Vec<String>) -> String {
-    let conv_strs: Vec<&str> = strs.iter().map(|s| &**s).collect();
+    let conv_strs: &Vec<&str> = &strs.iter().map(|s| &**s).collect();
+
     if conv_strs.is_empty() {
         return "".to_string();
     } else {
@@ -10,39 +11,41 @@ fn longest_common_prefix(strs: Vec<String>) -> String {
             min_len = cmp::min(min_len, str.len().try_into().unwrap());
         }
         let mut low = 1;
-        let mut high =  min_len;
+        let mut high = min_len;
 
         while low <= high {
             let middle = (low + high) / 2;
-            if isCommonPrefix(&conv_strs, &middle) {
+            if is_common_prefix(conv_strs, &middle) {
                 low = middle + 1;
-            }
-            else {
+            } else {
                 high = middle - 1;
-            }  
+            }
         }
-        return "".to_string();
+
+        return conv_strs[0]
+            .get(0..((high + low) / 2) as usize)
+            .unwrap()
+            .to_string();
     }
-    
 }
 
-fn isCommonPrefix(strs: &Vec<&str>, len: &i32) -> bool {
+fn is_common_prefix(strs: &Vec<&str>, len: &i32) -> bool {
     let str1: &str = &strs[0].get(0..*len as usize).unwrap();
-    
-    //let mut slice_strs: Vec<&str> = [].to_vec();
-    // strs[1..].clone_from_slice(&slice_strs);
-    for str in &strs[1..] {
+
+    for &str in &strs[1..] {
         if !str.starts_with(str1) {
             return false;
-        } 
-    } 
+        }
+    }
     return true;
 }
 
-
 fn main() {
-    let strs:Vec<String> = vec!["flower".to_string(),"flow".to_string(),"flight".to_string()];
+    let strs: Vec<String> = vec![
+        "flower".to_string(),
+        "flow".to_string(),
+        "flight".to_string(),
+    ];
 
     println!("{}", longest_common_prefix(strs));
-    
 }
